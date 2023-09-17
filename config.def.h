264d933c9169a6e01ecac0a10493fb500667fa5c
Jayanth Parthsarathy <jparthaa@gmail.com>
2023-09-17 15:13:09 +0530

    Apply status padding patch

--- config.def.h
+++ config.def.h
@@ -5,6 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 11;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 11;        /* vertical padding for statusbar */
 static const int vertpad            = 15;       /* vertical padding of bar */
 static const int sidepad            = 15;       /* horizontal padding of bar */
 static const char *fonts[]          = { "JetBrains Mono NerdFont:size=11" };
