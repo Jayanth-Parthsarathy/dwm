afc5841a25d55dca2a9ba0f8d242cc1af2f4ef11
Jayanth Parthsarathy <jparthaa@gmail.com>
2023-09-19 16:02:53 +0530

    Add gruvbox theme

---
+++ gruvbox.h
@@ -0,0 +1,12 @@
+static const char background[]   = "#282828";
+static const char bg_alt[]      = "#3c3836";
+static const char foreground[]   = "#ebdbb2";
+static const char cyan[]        = "#83a598";
+static const char green[]       = "#b8bb26";
+static const char orange[]      = "#fe8019";
+static const char pink[]        = "#d3869b";
+static const char purple[]      = "#b16286";
+static const char red[]         = "#fb4934";
+static const char yellow[]      = "#fabd2f";
+static const char blue[]        = "#83a598";
+static const char white[]       = "#ebdbb2";
