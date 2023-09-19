af5a7887b22ff2a863157ebbb5696a91a7d1ccbb
Jayanth Parthsarathy <jparthaa@gmail.com>
2023-09-19 21:28:53 +0530

    Change border color for gruvbox

--- gruvbox.h
+++ gruvbox.h
@@ -9,4 +9,4 @@ static const char purple[]      = "#b16286";
 static const char red[]         = "#fb4934";
 static const char yellow[]      = "#fabd2f";
 static const char blue[]        = "#83a598";
-static const char white[]       = "#ebdbb2";
+static const char white[]       = "#a89984";
